1162049d098f06c70d4e456f43889b4bf45803c9
bhuvankachhadiya <137286464+bhuvankachhadiya@users.noreply.github.com>
2023-06-28 16:45:06 +0530

    Add files via upload

---
+++ TERNARY3.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a=1,b=2,c=3;
+clrscr();
+(a>b)?(a>c)?printf("a is big"):printf("c is big")
+     :(b>c)?printf("b is big"):printf("c2 is big");
+
+
+getch();
+}
